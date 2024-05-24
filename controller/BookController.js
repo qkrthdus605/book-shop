43d83e63d50c13200e95ae8fa5fedb525f39fd09
qkrthdus605 <qkrthdus605@gmail.com>
2024-05-24 18:22:05 +0900

    feat: 도서 목록 조회 페이징 구현

--- controller/BookController.js
+++ controller/BookController.js
@@ -2,36 +2,42 @@ const conn = require("../mariadb");
 const { StatusCodes } = require("http-status-codes");
 
 const allBooks = (req, res) => {
-  let { category_id } = req.query;
+  let { category_id, news, limit, currentPage } = req.query;
 
-  // 쿼리가 있을 때와 없을 때로 분리
-  if (category_id) {
-    let sql = "SELECT * FROM books WHERE category_id=?";
+  // limit: 페이지 당 도서 수
+  // currentPage: 현재 몇 페이지인지
+  // offset: limit * (currentPage-1)
+  let offset = limit * (currentPage - 1);
+  let sql = "SELECT * FROM books";
+  let values = [];
 
-    conn.query(sql, category_id, (err, results) => {
-      if (err) {
-        console.log(err);
-        return res.status(StatusCodes.BAD_REQUEST).end();
-      }
-
-      if (results.length) {
-        return res.status(StatusCodes.OK).json(results);
-      } else {
-        return res.status(StatusCodes.NOT_FOUND).end();
-      }
-    });
-  } else {
-    let sql = "SELECT * FROM books";
-
-    conn.query(sql, (err, results) => {
-      if (err) {
-        console.log(err);
-        return res.status(StatusCodes.BAD_REQUEST).end();
-      }
-
-      return res.status(StatusCodes.OK).json(results);
-    });
+  if (category_id && news) {
+    sql +=
+      " WHERE category_id=? AND pub_date BETWEEN DATE_SUB(NOW(), INTERVAL 1 MONTH) AND NOW();";
+    values = [category_id];
+  } else if (category_id) {
+    sql += " WHERE category_id=?";
+    values = [category_id];
+  } else if (news) {
+    sql +=
+      " WHERE pub_date BETWEEN DATE_SUB(NOW(), INTERVAL 1 MONTH) AND NOW();";
   }
+
+  sql += " LIMIT ? OFFSET ?";
+  values.push(parseInt(limit), offset);
+
+  conn.query(sql, values, (err, results) => {
+    if (err) {
+      console.log(err);
+      return res.status(StatusCodes.BAD_REQUEST).end();
+    }
+
+    if (results.length) {
+      return res.status(StatusCodes.OK).json(results);
+    } else {
+      return res.status(StatusCodes.NOT_FOUND).end();
+    }
+  });
 };
 
 const bookDetail = (req, res) => {
@@ -39,7 +45,7 @@ const bookDetail = (req, res) => {
   // 비구조화 불가능
   let { id } = req.params;
 
-  let sql = "SELECT * FROM books WHERE id=?";
+  let sql = `SELECT * FROM books LEFT JOIN category ON books.category_id = category.id WHERE books.id=?;`;
 
   conn.query(sql, id, (err, results) => {
     if (err) {
